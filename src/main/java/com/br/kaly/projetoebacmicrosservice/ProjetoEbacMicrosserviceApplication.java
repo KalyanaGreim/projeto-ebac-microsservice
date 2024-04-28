67b9c02ec5e8a6a4a0ff2a9ae718c8510bc52eb7
KalyanaGreim <kalygreim11@gmail.com>
2024-04-28 16:06:23 -0300

    Initial commit

---
+++ src/main/java/com/br/kaly/projetoebacmicrosservice/ProjetoEbacMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package com.br.kaly.projetoebacmicrosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEbacMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEbacMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/kaly/projetoebacmicrosservice/ProjetoEbacMicrosserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.kaly.projetoebacmicrosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEbacMicrosserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
